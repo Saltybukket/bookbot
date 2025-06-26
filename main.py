f1d4e455ef824d657e85ee147ae1497cf463de1a
Saltybukket <tobias.feichtner.chr@gmail.com>
2025-06-27 01:08:19 +0200

    dictionary in order

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import count_words
 from stats import count_characters
+from stats import sort_dict
+from stats import print_book_stats
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -7,11 +9,19 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    book_contents = get_book_text("../bookbot/books/frankenstein.txt")
+    filepath = "../bookbot/books/frankenstein.txt"
+    book_contents = get_book_text(filepath)
     #print(book_contents)
-    print(f"\n{count_words(book_contents)} words found in the document")
-    char_dict = count_characters(book_contents)
-    print(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_contents)} total words")
+    
+    #char_dict = count_characters(book_contents)
+    #print(char_dict)
+    print("--------- Character Count -------")
+    sorted_dict = sort_dict(book_contents)
+    print_book_stats(sorted_dict)
     return 0
 
 main()
--- stats.py
+++ stats.py
@@ -13,7 +13,40 @@ def count_characters(book_text):
             dict[character.lower()] = 1
         else:
             dict[character.lower()] += 1                 
-    #print(dict) 
-    
     return dict
+
+#returns number in list section for each char
+def sort_criteria(dict_elements):   
+    return dict_elements["num"]
    
+def sort_dict(book_text):
+    unsorted_dict = count_characters(book_text)
+    list_of_dictionaries = []
+    #print(f"=========\nUnsorted:\n{unsorted_dict}\n==========")
+    #load list
+    for key in unsorted_dict:
+        dict_element = {
+                    "char": None,
+                    "num" : None
+                    }
+        dict_element["char"] = key
+        dict_element["num"] = unsorted_dict[key]
+        list_of_dictionaries.append(dict_element)
+        
+    #sort list high to low, sort criteria ["num"]
+    list_of_dictionaries.sort(reverse = True, key = sort_criteria)   
+    return list_of_dictionaries
+ 
+def print_book_stats(list_of_dictionaries):
+    for element in list_of_dictionaries:
+        if element["char"].isalpha():
+            print(f"{element['char']}: {element['num']}")
+        
+            
+    
+    
+    
+    
+    
+    
+    
