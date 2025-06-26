2cb5406da5a8fe71e75a45871ffe0b38e7ec8cc1
Saltybukket <tobias.feichtner.chr@gmail.com>
2025-06-27 01:40:46 +0200

    use user input for filepath

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from stats import count_words
 from stats import count_characters
 from stats import sort_dict
 from stats import print_book_stats
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -9,16 +10,22 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    filepath = "../bookbot/books/frankenstein.txt"
+    try:
+        filepath = sys.argv[1]
+    except Exception as e:
+        print(e)
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
     book_contents = get_book_text(filepath)
-    #print(book_contents)
+    print(book_contents)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(book_contents)} total words")
     
-    #char_dict = count_characters(book_contents)
-    #print(char_dict)
+    char_dict = count_characters(book_contents)
+    print(char_dict)
     print("--------- Character Count -------")
     sorted_dict = sort_dict(book_contents)
     print_book_stats(sorted_dict)
